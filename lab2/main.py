3e0281adeac10c695f0bb3e2581cfab013d32b0a
maciektr <mtratnowiecki@gmail.com>
2021-01-16 20:55:18 +0100

    Fix lab2

---
+++ lab2/main.py
@@ -0,0 +1,26 @@
+from math import sqrt
+from numpy.linalg import inv
+import numpy as np
+
+
+def ldlt(matrix):
+    result = [[0] * len(matrix) for _ in range(len(matrix))]
+    for i in range(len(matrix)):
+        for j in range(i+1):
+            # tmp_sum = sum(L[i][j] * L[k][j] for j in range(k))
+            tmp = 0
+            for k in range(j):
+                tmp += result[i][k] * result[j][k]
+            if (i == j):
+                result[i][j] = sqrt(matrix[i][i] - tmp)
+            else:
+                result[i][j] = (1 / result[j][j] * (matrix[i][j] - tmp))
+    c = np.array(result)
+    s = np.diag(np.diag(c))
+    return c @ inv(s), s @ s
+
+A = [[4, 12, -16],
+     [12, 37, -43],
+     [-16, -43, 98]]
+l,d = ldlt(A)
+print(l @ d @ l.T)
